ea63c6f520534ec97cc5345eabe4a798b48d7f2d
jaimersonsantos <jaimersonsantos@ufam.edu.br>
2020-02-01 15:52:23 -0400

    :construction: Tela de cadastrar

---
+++ web/src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./global.css";
+import "./App.css";
+import "./Siderbar.css";
+/*  */
+function App() {
+  return (
+    <>
+      <div id="app">
+        <aside>
+          <strong>Cadrastrar</strong>
+          <form>
+            <div className="input-block">
+              <label htmlFor="github_username">Usuário do Github</label>
+              <input id="github_username" name="github_username" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="techs">Tecnologias</label>
+              <input id="techs" name="github_username" required />
+            </div>
+
+            <div className="input-group">
+              <div className="input-block">
+                <label htmlFor="latitude">Latitude</label>
+                <input id="latitude" name="latitude" required />
+              </div>
+
+              <div className="input-block">
+                <label htmlFor="longitude">Longitude</label>
+                <input id="longitude" name="longitude" required />
+              </div>
+            </div>
+            <button type="submit">Salvar</button>
+          </form>
+        </aside>
+        <main></main>
+      </div>
+    </>
+  );
+}
+
+export default App;
